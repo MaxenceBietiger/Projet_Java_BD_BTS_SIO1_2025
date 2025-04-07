a00cbe5e5e64e34c8939eb709c65c626b2b0de02
Maxence <144607029+MaxenceBietiger@users.noreply.github.com>
2025-04-07 09:10:51 +0200

    Update Script Création DB.sql

--- Script SQL/Script Création DB.sql
+++ Script SQL/Script Création DB.sql
@@ -1,15 +1,4 @@
-# Modification de la table produits pour que chaque produit ait un fournisseur
-ALTER TABLE produits ADD COLUMN idFournisseur VARCHAR(255);
-
-# Modification de la table vente pour que chaque vente ait un produit
-ALTER TABLE vente ADD COLUMN idProduit INT;
-
-# Modification de la table produits pour que chaque produit ait une quantité
-ALTER TABLE produits ADD COLUMN quantite INT DEFAULT 0;
-
 create database DB_Projet_Java;
-
-DROP TABLE vente;
 
 CREATE TABLE produits (
     idProduits INT PRIMARY KEY AUTO_INCREMENT,
